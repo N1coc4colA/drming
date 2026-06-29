6edd158d031ec93fc8b7096a375dd7b36d8f3431
n1coc4cola <n1coc4cola@gmail.com>
2026-06-29 23:43:29 +0200

    Fix wrong maximumLPPTries name

--- settings.h
+++ settings.h
@@ -15,7 +15,7 @@ constexpr int numberCount(const int v)
 
 static constexpr int maximumDisplayCount = 999;
 static constexpr int maximumDisplayCountLength = utils::numberCount(maximumDisplayCount);
-static constexpr int maximumLPPTries = 3;
+static constexpr int maximumLPTries = 3;
 
 static constexpr int dtlsChunkSize = 1000;
 static constexpr int frameMSecsInterval = 60;
